50fb3c7041a3eb459d4a2741a1d75fcc69fe6436
zeyad <zeyad.altwalah@gmail.com>
2021-10-12 14:45:08 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+const val APPLES = 1000
+val DOZENS = 12
+fun main(args: Array<String>) {
+   var remainApples = APPLES%DOZENS
+    var no0fDozens= APPLES / DOZENS
+
+
+
+    println(no0fDozens)
+    println(remainApples)
+}
